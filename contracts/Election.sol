c87a15ca7eaff7eaa55a3449d64f82737f0c5663
roheat <rohithroxtar@gmail.com>
2018-10-23 23:48:07 +0530

    candidate feature added

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,10 +1,29 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-	// State Variable
-	string public candidate;
+	// Model a candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	// Map id to Candidates to store them
+	mapping(uint => Candidate) public candidates;
+
+	// Count the number of candidates
+	uint public candidatesCount;
+
 	// Constructor
 	function Election() public {
-		candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
 	}
+
+	// Add a new candidate
+	function addCandidate(string _name) private {
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
 }
